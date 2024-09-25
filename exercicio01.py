5a555720010b82e5b63aa7976bbbb2c9949de1b4
Nathan Henrique <ribeiro.nathan2002@gmail.com>
2024-09-25 13:27:58 -0300

    aulas python

---
+++ exercicio01.py
@@ -0,0 +1,13 @@
+nome = 'Nathan'
+sobrenome = 'Ribeiro'
+idade = 22
+ano_nascimento = 2002
+maior_idade = True
+altura_metros = 1.80
+
+print('Nome:', nome);
+print('Sobrenome:', sobrenome);
+print('Idade:', idade);
+print('Ano de nascimento:', ano_nascimento);
+print('É maior de idade?', maior_idade);
+print('Altura em metros:', altura_metros);
