c7ac20911e752e58a4cf9d7a1ce9638cec6f25ee
Nathan Henrique <ribeiro.nathan2002@gmail.com>
2024-09-26 13:17:23 -0300

    adicionando o exercicio 3

---
+++ exercicio03.py
@@ -0,0 +1,10 @@
+primeiro_valor = input('Digite um numero: ')
+segundo_valor = input('Digite outro numero: ')
+
+
+
+if primeiro_valor > segundo_valor:
+    print (f'{primeiro_valor} é maior que {segundo_valor} !')
+else:
+    print (f'{segundo_valor} é maior que {primeiro_valor} !')
+    
